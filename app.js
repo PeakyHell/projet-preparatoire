7ed407acb3dfd1cf8a99e3b9dc9423d739ad6c70
PeakyHell <louis.vanimpe@student.uclouvain.be>
2024-10-01 13:28:04 +0200

    Application NodeJS basique

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', 'public/templates')
+app.set()
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.listen(3000)
