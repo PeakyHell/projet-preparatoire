0118ae9a61f5fe0d2426bb98b16aee5ee00baad7
PeakyHell <louis.vanimpe@student.uclouvain.be>
2024-10-15 10:09:40 +0200

    Fixed links to other pages

--- app.js
+++ app.js
@@ -9,4 +9,12 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.get('/auth', (req, res) => {
+    res.render('auth')
+})
+
+app.get('/report', (req, res) => {
+    res.render('report')
+})
+
 app.listen(3000)
