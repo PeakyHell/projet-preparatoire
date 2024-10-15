b392456ff7485d372c4487be94a9e1a96e58682f
PeakyHell <louis.vanimpe@student.uclouvain.be>
2024-10-15 09:42:41 +0200

    Modification des liens de la feuille css et des autres pages

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', 'public/templates')
-app.set()
+app.use(express.static('public/static'))
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index')
 })
 
 app.listen(3000)
