eedba59eb3a9e69a8b7f7fe051917c26415499cd
PeakyHell <louis.vanimpe@student.uclouvain.be>
2024-11-12 08:41:03 +0100

    Ajout de redirections en fonction de si l'utilisateur est connecté ou non en utilisant les sessions

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const session = require('express-session')
 const app = express()
+
 
 app.set('view engine', 'ejs')
 app.set('views', 'public/templates')
 app.use(express.static('public/static'))
+
+
+app.use(session({
+    secret: 'propre123'
+}));
+
 
 app.get('/', (req, res) => {
     res.render('base', {
@@ -13,17 +21,27 @@ app.get('/', (req, res) => {
 })
 
 app.get('/auth', (req, res) => {
-    res.render('base', {
-        title: 'Auth',
-        content: 'auth'
-    })
+    if (req.session.username) {
+        res.redirect('/')
+    }
+    else {
+        res.render('base', {
+            title: 'Auth',
+            content: 'auth'
+        })
+    }
 })
 
 app.get('/report', (req, res) => {
-    res.render('base', {
-        title: 'Report',
-        content: 'report'
-    })
+    if (req.session.username) {
+        res.render('base', {
+            title: 'Report',
+            content: 'report'
+        })
+    }
+    else {
+        res.redirect('/auth')
+    }    
 })
 
 app.listen(3000)
