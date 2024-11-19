9a5ccf3b3d4e47912712cc5ca4c41cce36c916d5
Valentinlambert1 <valentin.lambert@student.uclouvain.be>
2024-11-19 23:52:05 +0100

    Update config.js
    config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    db_uri: "",
-    db_name: "",
-    session_secret: "",
-    server_passphrase: ""
+    db_uri: "mongodb+srv://louis:V89qT4AG0oHSaF0J@projet-prepa.4iswn.mongodb.net/?retryWrites=true&w=majority&appName=Projet-Prepa",
+    db_name: "Projet",
+    session_secret: "secret123",
+    server_passphrase: "ingi"
 }
