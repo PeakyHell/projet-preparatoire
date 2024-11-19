9d773dc4ee49c7f4e0637789356275c7788a9910
PeakyHell <louis.vanimpe@student.uclouvain.be>
2024-11-19 22:03:00 +0100

    db tests

--- tests/db.test.js
+++ tests/db.test.js
@@ -2,14 +2,15 @@ const db_uri = require('../config').db_uri
 const incidentsCollection = require('../modules/db').incidentsCollection
 const usersCollection = require('../modules/db').usersCollection
 
-describe('Database Connection', () => {
-    test('Valid Database URI', async () => {
+describe('Connexion à la base de données', () => {
+    it('URI de la base de données valide', async () => {
         expect(
             db_uri.startsWith('mongodb://') || db_uri.startsWith('mongodb+srv://')
         ).toBe(true)
     })
 
-    test('Database contains Users and Incidents collections', async () => {
-        
+    it('la base de donnée doit contenir une collection Users et une collection Incidents', async () => {
+        expect(incidentsCollection).toBeDefined()
+        expect(usersCollection).toBeDefined()
     })
 })
