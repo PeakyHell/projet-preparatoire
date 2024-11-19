60d5039c24dc2c37354109afd522e88bc7076d74
DelArthur1 <150275516+DelArthur1@users.noreply.github.com>
2024-11-19 22:29:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
-module.exports = {
-    db_uri: "mongodb+srv://arthur:gsRjg6ly9w4meGVH@projet-prepa.4iswn.mongodb.net/?retryWrites=true&w=majority&appName=Projet-Prepa",
-    db_name: "Projet",
-    session_secret: "secret123",
-    server_passphrase: "ingi"
-}
+
